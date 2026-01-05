87cc2858cf2cbaaef8ee898cc5389c3ca39a22e1
SnO2WMaN <me@sno2wman.net>
2026-01-06 04:24:59 +0900

    test

--- update_mylists.sh
+++ update_mylists.sh
@@ -5,7 +5,12 @@ fetch_nicovideo_mylist() {
   local filename="nicovideo_${mylist_id}.txt"
  
   for page in {1..5}; do
-    curl -s -A "Googlebot/2.1" "https://nvapi.nicovideo.jp/v2/mylists/${mylist_id}?pageSize=100&page=${page}&_frontendId=6&_frontendVersion=0" | jq -r ".data.mylist.items.[].watchId" >> "$filename"
+    http -hb "https://nvapi.nicovideo.jp/v2/mylists/${mylist_id}" \
+      pageSize==1 \
+      page==${page} \
+      _frontendId==6 \
+      _frontendVersion==0 \
+      User-Agent:"Googlebot/2.1"
   done
 
   cat "$filename" | sort | uniq | sponge "$filename"
