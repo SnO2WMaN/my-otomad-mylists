412d421a858b1fd29ff28beab7a0622217b91a65
SnO2WMaN <me@sno2wman.net>
2026-01-06 02:28:46 +0900

    init

---
+++ get_urls.sh
@@ -0,0 +1,5 @@
+yt-dlp --flat-playlist --get-url "https://www.nicovideo.jp/my/mylist/77541320" | sort > nicovideo_77541320.txt
+yt-dlp --flat-playlist --get-url "https://www.nicovideo.jp/my/mylist/78076337" | sort > nicovideo_78076337.txt
+yt-dlp --flat-playlist --get-url "https://www.nicovideo.jp/my/mylist/78110237" | sort > nicovideo_78110237.txt
+yt-dlp --flat-playlist --get-url "https://www.nicovideo.jp/my/mylist/78982639" | sort > nicovideo_78982639.txt
+yt-dlp --flat-playlist --get-url "https://www.nicovideo.jp/my/mylist/79112044" | sort > nicovideo_79112044.txt
