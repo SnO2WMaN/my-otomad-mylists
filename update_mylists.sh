8f3717093bb7006022510fcb866d6bdcd91c511b
SnO2WMaN <me@sno2wman.net>
2026-01-06 02:44:45 +0900

    Add all

--- update_mylists.sh
+++ update_mylists.sh
@@ -3,3 +3,5 @@ yt-dlp --flat-playlist --get-url "https://www.nicovideo.jp/my/mylist/78076337" |
 yt-dlp --flat-playlist --get-url "https://www.nicovideo.jp/my/mylist/78110237" | sort > nicovideo_78110237.txt
 yt-dlp --flat-playlist --get-url "https://www.nicovideo.jp/my/mylist/78982639" | sort > nicovideo_78982639.txt
 yt-dlp --flat-playlist --get-url "https://www.nicovideo.jp/my/mylist/79112044" | sort > nicovideo_79112044.txt
+
+cat nicovideo_*.txt | sort | uniq > all.txt
